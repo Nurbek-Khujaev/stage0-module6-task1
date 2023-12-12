aec4b55d35917b5a99e0068d205290dccb0a5d7c
nurbekkhujaev <nurbek.khuja@gmail.com>
2023-12-12 18:17:55 +0500

    bugs fixed

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,43 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    public Bird(String color, int numberOfPaws, boolean hasFur) {
+        super("blue", 2, false);
+    }
 
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    public void setColor(String color) {
+        super.setColor("blue");
+
+    }
+
+    public int getNumberOfPaws() {
+        return super.getNumberOfPaws();
+    }
+
+    public void setNumberOfPaws(int numberOfPaws) {
+        super.setNumberOfPaws(2);
+    }
+
+    public boolean getHasFur() {
+        return super.getHasFur();
+    }
+
+    public void setHasFur(boolean hasFur) {
+        super.setHasFur(false);
+    }
+
+
+    public String getDescription() {
+        if (this.getHasFur()) {
+            return "This animals is mostly " + getColor() + ". It has " + getNumberOfPaws() + " paws and fur. Moreover, it has 2 wings and can fly.";
+        } else {
+            return "This animals is mostly " + getColor() + ". It has " + getNumberOfPaws() + " paws and no fur. Moreover, it has 2 wings and can fly.";
+        }
+    }
 }
+
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,43 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal {
+    public Dog(String color, int numberOfPaws, boolean hasFur) {
+        super("brown", 4, true);
+    }
 
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    public void setColor(String color) {
+        super.setColor("blue");
+
+    }
+
+    public int getNumberOfPaws() {
+        return super.getNumberOfPaws();
+    }
+
+    public void setNumberOfPaws(int numberOfPaws) {
+        super.setNumberOfPaws(2);
+    }
+
+    public boolean getHasFur() {
+        return super.getHasFur();
+    }
+
+    public void setHasFur(boolean hasFur) {
+        super.setHasFur(false);
+    }
+
+
+    public String getDescription() {
+        if (this.getHasFur()) {
+            return "This animals is mostly " + getColor() + ". It has " + getNumberOfPaws() + " paws and fur.";
+        } else {
+            return "This animals is mostly " + getColor() + ". It has " + getNumberOfPaws() + " paws and no fur.";
+        }
+    }
 }
+
