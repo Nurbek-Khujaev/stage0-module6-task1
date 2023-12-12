96fcc4b986aaa07a19e1e6467928971a4bfab32b
nurbekkhujaev <nurbek.khuja@gmail.com>
2023-12-12 15:18:31 +0500

    bugs fixed

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,40 @@
 package com.epam.mjc.stage0;
 
+import java.awt.*;
+
 public class Animal {
+
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public void Color(String color) {
+
+        System.out.println(color);
+
+    }
+
+    public void NumberOfPaws(int numberOfPaws) {
+
+        System.out.println(numberOfPaws);
+
+    }
+
+    public void HasFur(boolean hasFur) {
+
+        System.out.println(hasFur);
+
+    }
+
+
+    public String getDescription() {
+
+        if (hasFur) {
+            return "This animal is mostly " + color + ". It has " + numberOfPaws + " and a fur";
+        } else {
+            return "This animal is mostly " + color + ". It has " + numberOfPaws + " and no fur";
+        }
+
+    }
 
 }
